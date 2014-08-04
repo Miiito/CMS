b1e53593e1e16f78fee0a80c5bcb9db9a1d2a161
Nikola Kovacs <n.kovacs@mito.hu>
2014-08-04 13:01:48 +0200

    coding standards

--- tests/_helpers/PhpBrowserAuthHelper.php
+++ tests/_helpers/PhpBrowserAuthHelper.php
@@ -1,10 +1,11 @@
 <?php
 namespace Codeception\Module;
 
-class PhpBrowserAuthHelper extends PhpBrowser {
-    public function _before(\Codeception\TestCase $test) {
+class PhpBrowserAuthHelper extends PhpBrowser
+{
+    public function _before(\Codeception\TestCase $test)
+    {
         parent::_before($test);
         $this->goutte->setAuth($this->config['auth'][0], $this->config['auth'][1]);
     }
 }
-?>
