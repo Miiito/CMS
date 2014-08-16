4c9342a351b46b94aa2204568f3fb5b068cd0d77
Alexander Makarov <sam@rmcreative.ru>
2014-08-16 18:38:56 +0400

    Exposed basic app entry URL via codeception.yml, refactored configs of advanced app to read from config and define entry script in a single place
    Conflicts:
    	tests/codeception.yml
    	tests/codeception/_bootstrap.php

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -1,19 +1,13 @@
 <?php
-
-include('_bootstrap.local.php');
-
-// the entry script file path for functional and acceptance tests
-defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(dirname(__DIR__)) . '/testweb/index-test.php');
-
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
+defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', \Codeception\Configuration::config()['config']['test_entry_url']);
+defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(dirname(__DIR__)) . '/testweb/index-test.php');
 
+require_once(__DIR__ . '/../../vendor/autoload.php');
 require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
-// set correct script paths
 $_SERVER['SCRIPT_FILENAME'] = TEST_ENTRY_FILE;
 $_SERVER['SCRIPT_NAME'] = TEST_ENTRY_URL;
 $_SERVER['SERVER_NAME'] = 'localhost';
