45788a60c038d7e6aad541b7e871c602198e4da1
Nikola Kovacs <n.kovacs@mito.hu>
2014-10-14 16:18:16 +0200

    fix exit code

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-git submodule update --init
-composer install --prefer-dist
-npm install
-grunt postinstall --environment="development" --email="noreply@mito.hu"
-grunt testroot --testroot=/me/yii2/basic
+git submodule update --init && composer install --prefer-dist && npm install && grunt postinstall --environment="development" --email="noreply@mito.hu" && grunt testroot --testroot=/me/yii2/basic
+ec=$?
+if [ $ec -ne 0 ]; then
+	exit $ec
+fi
 grunt test
+ec1=$?
 grunt phpcs
+ec2=$?
+
+if [ $ec1 -ne 0 ] || [ $ec2 -ne 0 ]; then
+	exit 1
+fi
+exit 0
