aa96747a4c7228354943e1c3f0abc196d2180405
Nikola Kovacs <n.kovacs@mito.hu>
2014-10-14 19:14:50 +0200

    lightweight test & php for cirunner

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 dir=$(basename $(pwd))
-git submodule update --init && composer install --prefer-dist && npm install && gulp postinstall --environment="development" --email="noreply@mito.hu" && gulp testroot --testroot="/cirunner/$dir"
+git submodule update --init && composer install --prefer-dist && php setup.php development && php testsetup.php "noreply@mito.hu" "/cirunner/$dir"
 ec=$?
 if [ $ec -ne 0 ]; then
 	exit $ec
 fi
-gulp test
+cd tests
+../vendor/bin/codecept build && ../vendor/bin/codecept run
 ec1=$?
-gulp phpcs
+cd ..
+
+npm install glob-stream through2
+node phpcs.js
 ec2=$?
 
-if [ $ec1 -ne 0 ] || [ $ec2 -ne 0 ]; then
+if [ $ec1 -ne 0 ] || [ $ec2 -ne 0 ] then
 	exit 1
 fi
 exit 0
