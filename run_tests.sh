4c405742151d789966defefeec237799c3f7f9f0
Nikola Kovacs <n.kovacs@mito.hu>
2014-10-14 18:16:14 +0200

    run_tests gulp

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 dir=$(basename $(pwd))
-git submodule update --init && composer install --prefer-dist && npm install && grunt postinstall --environment="development" --email="noreply@mito.hu" && grunt testroot --testroot="/cirunner/$dir"
+git submodule update --init && composer install --prefer-dist && npm install && gulp postinstall --environment="development" --email="noreply@mito.hu" && gulp testroot --testroot="/cirunner/$dir"
 ec=$?
 if [ $ec -ne 0 ]; then
 	exit $ec
 fi
-grunt test
+gulp test
 ec1=$?
-grunt phpcs
+gulp phpcs
 ec2=$?
 
 if [ $ec1 -ne 0 ] || [ $ec2 -ne 0 ]; then
