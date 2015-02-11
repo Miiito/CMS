31b71007adc13c781a8f4a3b1c520beb28c8f463
Barnabas Szekeres <szekeresbarnabas@gmail.com>
2015-02-11 17:27:22 +0100

    basic tests

--- tests/protractor/config.js
+++ tests/protractor/config.js
@@ -1,16 +1,17 @@
+var localConfig = require('../config.json');
+
+console.log(localConfig);
+
 exports.config = {
-	seleniumAddress: 'http://192.168.62.92:4444/wd/hub',
-	//seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar',
+	seleniumAddress: localConfig.seleniumAddress || 'http://192.168.88.24:4444/wd/hub',
 
 	// Capabilities to be passed to the webdriver instance.
 	capabilities: {
-		'browserName': 'chrome'
+		'browserName': localConfig.browser || 'phantomjs'
 	},
 
 	params: {
-		username: 'mito',
-		password: 'mito',
-		baseUrl: 'http://mito:mito@dev.mito.hu/bszekeres/basic-template/web/'
+		baseUrl: 'http://mito:mito@playground/bszekeres/basic-template/testweb/index-test.php'
 	},
 
 	// Options to be passed to Jasmine-node.
--- tests/protractor/tests/sitemap.js
+++ tests/protractor/tests/sitemap.js
@@ -1,33 +1,45 @@
 describe('Sitemap checking', function() {
 
 	/* If we want test a non angular page */
-	browser.ignoreSynchronization = true;
+
+	beforeEach(function() {
+		browser.ignoreSynchronization = true;
+	 	browser.manage().window().setSize(800,600);
+		//browser.manage().timeouts().implicitlyWait(2000);
+  	});
 
 	it('Az oldal címének meg kellett volna jellenie', function() {
 		browser.get(browser.params.baseUrl);
 		var greeting = element(by.css('.jumbotron h1'));
 		expect(greeting.getText()).toEqual('Congratulations!');
-		expect(browser.getCurrentUrl()).toBe(browser.params.baseUrl);
 	});
 
 	it('About oldalnak meg kell kellenie', function() {
-		element.all(by.css('.navbar-nav li')).get(1).click();
-		//browser.pause();
-		/*
-			browser.get(browser.params.baseUrl);
-			var greeting = element(by.css('.jumbotron h1'));
-			expect(greeting.getText()).toEqual('Congratulations!');
-		*/
+    	element.all(by.css('.navbar-nav li a')).get(1).click();
+       	var greeting = element(by.css('.container h1'));
+		expect(greeting.getText()).toEqual('About');
 	});
 
-	it('Contact oldalnak meg kell kellenie', function() {
-		element.all(by.css('.navbar-nav li')).get(2).click();
-		browser.pause();
-		/*
-			browser.get(browser.params.baseUrl);
-			var greeting = element(by.css('.jumbotron h1'));
-			expect(greeting.getText()).toEqual('Congratulations!');
-		*/
+	it('Login oldalnak meg kell kellenie', function() {
+		element.all(by.css('.navbar-nav li a')).get(3).click();
+
+		element(by.id('loginform-username')).sendKeys('admin');
+		element(by.id('loginform-password')).sendKeys('admin\n');
+
+		/* Maximum timeout for the login */
+		browser.manage().timeouts().implicitlyWait(5000);
+
+		var greeting = element(by.css('.jumbotron h1'));
+		expect(greeting.getText()).toEqual('Congratulations!');
+
+		/* Reset the timeout for the login */
+		browser.manage().timeouts().implicitlyWait(0);
+	});
+
+	it('About oldalnak meg kell kellenie', function() {
+		element.all(by.css('.navbar-nav li a')).get(1).click();
+		var greeting = element(by.css('.container h1'));
+		expect(greeting.getText()).toEqual('About');
 	});
 
 
