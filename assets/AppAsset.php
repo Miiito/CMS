422299eb93fd374ad1f91dc34722a6aac54a259f
Nikola Kovacs <n.kovacs@mito.hu>
2015-03-09 11:54:40 +0100

    add documentation and missing properties to AppAsset

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -13,22 +13,93 @@ namespace app\assets;
  */
 class AppAsset extends \mito\assets\AssetBundle
 {
+    /**
+     * @var string the base directory for development assets
+     *
+     * You can use either a directory or an alias of the directory.
+     */
     public $devPath = '@app/assets/main/src';
+
+    /**
+     * @var string the base directory for production assets
+     *
+     * You can use either a directory or an alias of the directory.
+     */
     public $distPath = '@app/assets/main/dist';
+
+    /**
+     * @var string relative path to images
+     *
+     * Images in this directory will be optimized and copied to the production path
+     * by the build process
+     */
     public $imgPath = null;
+
+    /**
+     * @var string relative path to fonts
+     *
+     * Files in this directory will be copied to the production path
+     * by the build process
+     */
+    public $fontPath = null;
+
+    /**
+     * @var array relative paths to css source files (scss, less etc.)
+     *
+     * files in these directory will be compiled to css files in the css directory
+     */
     public $cssSourcePaths = ['scss'];
+
+    /**
+     * @var array list of production css files
+     */
     public $css = [
         'css/site.css'
     ];
+
+    /**
+     * @var array|null list of development css files
+     * If this is not null, it will overwrite $css
+     */
+    public $devCss = null;
+
+    /**
+     * @var array list of development js files
+     * If this is not null, it will overwrite $js
+     * If an element is an array, the javascript files in that array
+     * will be compiled to the javascript file specified in the element's key
+     *
+     * For example:
+     *
+     * ```php
+     * public $devJs = [
+     *     'js/combined.js' => [ 'js/file1.js', 'js/file2.js' ],
+     * ];
+     * ```
+     *
+     */
     public $devJs = [
         'js/combined.js' => [ 'js/someplugin.js', 'js/site.js' ],
     ];
+
+    /**
+     * @var array list of production js files
+     */
     public $js = [
         'js/combined.js',
     ];
+
+    /**
+     * @var array list of dependent asset bundles
+     */
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
+    /**
+     * @var array|null extra parameters to pass to gulp.
+     */
+    public $extraParams;
 }
